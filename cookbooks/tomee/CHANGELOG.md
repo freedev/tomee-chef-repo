cbb3bc6602c3c0e381ce84a3bb6715433d8f549c
root <root@chef-tomee-berkshelf>
2014-12-30 01:24:36 +0000

    first import

--- cookbooks/tomee/CHANGELOG.md
+++ cookbooks/tomee/CHANGELOG.md
@@ -1,13 +1,3 @@
-tomee CHANGELOG
-===============
+# 0.1.0
 
-This file is used to list changes made in each version of the tomee cookbook.
-
-0.1.0
------
-- [your_name] - Initial release of tomee
-
-- - -
-Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
-
-The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
+Initial release of tomee
--- cookbooks/tomee/metadata.rb
+++ cookbooks/tomee/metadata.rb
@@ -1,30 +1,7 @@
 name             'tomee'
-maintainer       "Vincenzo D'Amore"
-maintainer_email 'v.damore@gmail.com'
-license          'Apache 2.0'
-description      'Installs/Configures Apache Tomee'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures tomee'
+long_description 'Installs/Configures tomee'
 version          '0.1.0'
-
-depends 'ark',  '~> 0.4'
-depends 'java', '~> 1.13'
-
-%w{
-    debian
-    ubuntu
-    centos
-    redhat
-    scientific
-    fedora
-    amazon
-    arch
-    oracle
-    freebsd
-    windows
-    suse
-    xenserver
-    smartos
-}.each do |os|
-  supports os
-end
-
